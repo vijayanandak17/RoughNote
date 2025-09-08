faa1e62cdd7c39f6898358889ab5ad432ef453df
vijayanandak17 <vijayanandak17@gmail.com>
2025-09-08 08:35:01 +0530

    Updated file sent to GITHUB

--- huggingface.py
+++ huggingface.py
@@ -1,4 +1,5 @@
 from transformers import pipeline  
 generator = pipeline("text-generation", model="gpt2") 
-result = generator("Once upon a time", max_length=30, num_return_sequences=1)
+result = generator("Once upon a time", max_length=5, num_return_sequences=1)
 print (result[0]['generated_text'])
+#done
